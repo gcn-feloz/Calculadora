77747cdd223f91acafcfd344bc6cb1513af26a9b
gcn-feloz <albertomacielkraemer@gmail.com>
2022-10-04 17:39:18 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main { 
+    
+public static void main(String[] args)
+{
+    Calculadora.soma(5,3);
+    Calculadora.subtrai(23,14);
+    Calculadora.multiplicacao(10,3);
+    Calculadora.divisão(50,5);
+}
+
+}
